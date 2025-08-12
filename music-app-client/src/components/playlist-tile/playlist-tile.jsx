12da6ebb8e4ccdc60d74237325192185bd245b74
JMaxRowe <jmaxrowe@gmail.com>
2025-08-12 15:55:16 +0100

    fixed unknown owner issue

--- music-app-client/src/components/playlist-tile/playlist-tile.jsx
+++ music-app-client/src/components/playlist-tile/playlist-tile.jsx
@@ -4,10 +4,11 @@ export default function PlaylistTile({playlist}) {
     return(
         <div className='playlistCard'>
             <div className='coverArt'>
-                
+
             </div>
             <div className='playlistInfo'>
                 <p>{playlist.title}</p>
+                <p>{playlist.owner?.username || "Unknown"}</p>
             </div>
         </div>
     )
