211f67453945f4456097460a39d3019298a4d989
Vivien <bihuiy7@gmail.com>
2025-08-12 22:49:13 +1000

    Revert "add homepage.jsx and services/homepage.js"
    This reverts commit db0b5471b5e218a23d590c3aa3510574af2e5e74.

--- music-app-client/src/components/views/Homepage/homepage.jsx
+++ music-app-client/src/components/views/Homepage/homepage.jsx
@@ -1,50 +1,10 @@
-import { useEffect, useState } from "react";
-import "./homepage.css";
-import { homePage } from "../../../services/homepage";
 
-export default function Homepage() {
-  // * State
-  const [topPlaylists, setTopPlaylists] = useState([]);
-  const [topSongs, setTopSongs] = useState([]);
-  //const [errors, setErrors] = useState(null);
-
-  useEffect(() => {
-    const getHomepageData = async () => {
-      try {
-        const { data } = await homePage();
-        setTopPlaylists(data.topPlaylists);
-        setTopSongs(data.topSongs);
-      } catch (error) {
-        console.log(error);
-        //setErrors(error);
-      }
-    };
-    getHomepageData();
-  }, []);
-
-  return (
-    <>
-      <h1>Music Player Homepage</h1>
-      <h2>Check out our top playlists:</h2>
-      <ul>
-        {topPlaylists.map((playlist) => (
-          <li key={playlist._id}>
-            <img src={playlist.coverArt} alt={`${playlist.title} cover`} />
-            <p>
-              {playlist.title} created by:{" "}
-              {playlist.owner?.username || "Unknown"}
-            </p>
-          </li>
-        ))}
-      </ul>
-      <h2>Check out our top songs:</h2>
-      <ul>
-        {topSongs.map((song) => (
-          <li key={song._id}>
-            {song.title} by {song.artist}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+const Homepage = () =>{
+    return(
+        <>
+            <h1>Music Player Homepage</h1>
+        </>
+    )
 }
+
+export default Homepage
