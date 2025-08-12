84be96eada1b7435195ab45247611ef1226d5410
JMaxRowe <jmaxrowe@gmail.com>
2025-08-12 12:59:47 +0100

    redirect signed in user to homepage if they go to sign-in page

--- music-app-client/src/components/views/user/sign-in/sign-in.jsx
+++ music-app-client/src/components/views/user/sign-in/sign-in.jsx
@@ -1,7 +1,13 @@
 import './sign-in.css'
 import SignInForm from '../../../sign-in-form/sign-in-form.jsx'
+import { useContext } from 'react'
+import { UserContext } from '../../../../contexts/userContext.jsx'
+import { Navigate } from 'react-router'
 
 export default function SignInPage(){
+    const {user} = useContext(UserContext)
+    if (user) return <Navigate to="/" />
+
     return(
         <main>
             <section className="form-section">
