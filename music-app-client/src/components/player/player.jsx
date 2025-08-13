9da263081fbc96688ad92c8a5fd95b03d9034b19
JMaxRowe <jmaxrowe@gmail.com>
2025-08-13 16:47:58 +0100

    added volume controls and progress bar

--- music-app-client/src/components/player/player.jsx
+++ music-app-client/src/components/player/player.jsx
@@ -1,22 +1,48 @@
 import './player.css'
 import { useAudioPlayerContext } from "react-use-audio-player";
-import { FaPlay, FaPause } from "react-icons/fa";
+import { FaPlay, FaPause, } from "react-icons/fa";
+import { GoMute, GoUnmute } from 'react-icons/go'
+import { useEffect, useState } from 'react';
 
 
 
 const Player = () => {
-const {togglePlayPause, isPlaying, src, load, seek, getPosition, setVolume, toggleMute} = useAudioPlayerContext()
-    const handleButtonClick = () =>{
+const {togglePlayPause, isPlaying, src, load, seek, getPosition, setVolume, toggleMute, isMuted, duration} = useAudioPlayerContext()
+    
+    const[position, setPosition] = useState(0)
+
+    useEffect(() => {
+        if(!src) {setPosition(0); return}
+        const id = setInterval(() => {
+            setPosition(getPosition())
+        }, 250)
+    }, [src])
+
+const handlePlayButtonClick = () =>{
         if (src) return togglePlayPause()
     }
+
+    const handleSeek = (e) => {
+        const newTime = e.target.value
+        seek(newTime)
+        setPosition(newTime)
+    }
+
+
     return(
-        <>
-        <div className="songInfo"></div>
-        <div className="playerControls">
-            <button className="togglePause"onClick={handleButtonClick}>{isPlaying ? <FaPause /> : <FaPlay/>} </button>
+        <div className='playerBar'>
+            <div className="songInfo"></div>
+            <div className="playerControls">
+                <div className="controlButtons">
+                    <button className="togglePause" onClick={handlePlayButtonClick}>{isPlaying ? <FaPause /> : <FaPlay/>} </button>
+                </div>
+                    <input type="range" min={0} max={duration} step={0.01} value={position} onChange={handleSeek} className="progressBar"  />
+            </div>
+            <div className="volumeControls">
+                <button className='muteButton' onClick={(toggleMute)}>{isMuted ? <GoMute /> : <GoUnmute />}</button>
+                <input type="range" min={0} max={1} step={0.01} className="volumeBar" onChange={(e) => setVolume(parseFloat(e.target.value))}/>
+            </div>
         </div>
-        <div className="volumeControls"></div>
-        </>
     )
 }
 
