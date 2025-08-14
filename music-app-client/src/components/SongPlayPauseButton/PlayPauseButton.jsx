490e82b006fdb7de8b8bb098d6f750a6d69bd284
JMaxRowe <jmaxrowe@gmail.com>
2025-08-14 21:50:53 +0100

    play button navigates to sign up if you aren't signed in

--- music-app-client/src/components/SongPlayPauseButton/PlayPauseButton.jsx
+++ music-app-client/src/components/SongPlayPauseButton/PlayPauseButton.jsx
@@ -1,12 +1,18 @@
 import { useAudioPlayerContext } from "react-use-audio-player";
 import { usePlayer } from "../../contexts/playerContext";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
+import { useNavigate } from "react-router";
+
 
 export default function PlayPauseButton({song, songs, index, url}){
   const { setPlaylist, setCurrentIndex } = usePlayer();
   const { load, togglePlayPause, isPlaying, src } = useAudioPlayerContext()
+  const { user } = useContext(UserContext)
+  const navigate = useNavigate()
 
     function handlePlayButton() {
-    
+      if (!user._id) return navigate("/user/sign-up")
       if (src === url){
         return togglePlayPause()
       }
