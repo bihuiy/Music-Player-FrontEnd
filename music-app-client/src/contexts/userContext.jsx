7386ae63ef0c930c632f4e7aff6a0c6025b5af94
JMaxRowe <jmaxrowe@gmail.com>
2025-08-14 22:00:02 +0100

    fixed sign out bug where reload signs you back in

--- music-app-client/src/contexts/userContext.jsx
+++ music-app-client/src/contexts/userContext.jsx
@@ -1,10 +1,19 @@
 import {createContext, useState} from 'react'
 import { getUser } from '../utils/auth'
+import { useEffect } from 'react'
 
 const UserContext = createContext(null)
 
 const UserProvider = ({children}) =>{
     const [user, setUser] = useState(getUser())
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        if (!token) {
+            setUser(null)
+            localStorage.removeItem("user")
+            return
+        }
+}, [])
     return(
         <UserContext.Provider value = {{user, setUser}}>
             {children}
