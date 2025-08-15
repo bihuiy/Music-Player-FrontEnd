33c6876b3c6d524c140d22e43130dd7a6b1904da
JMaxRowe <jmaxrowe@gmail.com>
2025-08-15 10:15:24 +0100

    fixed some syntax but bug continues

--- music-app-client/src/contexts/userContext.jsx
+++ music-app-client/src/contexts/userContext.jsx
@@ -6,24 +6,29 @@ import axios from "axios";
 const UserContext = createContext(null);
 
 const UserProvider = ({ children }) => {
-  const t = localStorage.getItem("token");
-  const [user, setUser] = useState(t ? getUser() : null);
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-      setUser(getUser());
+    const t = localStorage.getItem("token");
+    if (t) {
+        axios.defaults.headers.common.Authorization = `Bearer ${t}`
     } else {
-      delete axios.defaults.headers.common.Authorization;
-      setUser(null);
-      localStorage.removeItem("user");
+        delete axios.defaults.headers.common.Authorization
     }
-  }, []);
-  return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {children}
-    </UserContext.Provider>
-  );
+    const [user, setUser] = useState(t ? getUser() : null);
+    
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+        setUser(getUser());
+        } else {
+        delete axios.defaults.headers.common.Authorization;
+        setUser(null);
+        }
+    }, []);
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+        {children}
+        </UserContext.Provider>
+    );
 };
 
 export { UserContext, UserProvider };
