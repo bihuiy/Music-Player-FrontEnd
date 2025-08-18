a31fa0cae8b97d38a31c15473b144447c8cd8f4e
Vivien <bihuiy7@gmail.com>
2025-08-18 18:01:00 +1000

    rename the PlayerContext

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -4,14 +4,13 @@ import { getUser } from "../utils/auth";
 const UserContext = createContext(null);
 
 const UserProvider = ({ children }) => {
-    
-    const [user, setUser] = useState( getUser());
-    
-    return (
-        <UserContext.Provider value={{ user, setUser }}>
-        {children}
-        </UserContext.Provider>
-    );
+  const [user, setUser] = useState(getUser());
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export { UserContext, UserProvider };
---
+++ src/contexts/ty.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+const PlayerContext = createContext();
+
+const PlayerProvider = ({ children }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [playlist, setPlaylist] = useState([]);
+
+  return (
+    <PlayerContext.Provider
+      value={{ currentIndex, setCurrentIndex, playlist, setPlaylist }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+};
+
+export const usePlayer = () => useContext(PlayerContext);
+export { PlayerProvider };
